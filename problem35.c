e7c172d5d932a5691fe48550c9cc911e7648a326
Lornop <lorenoelectric@gmail.com>
2025-11-26 20:52:33 -0800

    Prob 35. Need to figure out how to do some number combination logic to test circular part

--- problem35.c
+++ problem35.c
@@ -16,13 +16,18 @@ How many circular primes are there below one million?
 void allDone(int);
 void foundOne(int);
 int primecheck(int);
-int circlecheck(int);
+int circlecheck(int);   //This one is big
+int numPlaces(int);
+int factorial(int);
+void singledigits(int, int);
+int digitsToInt(int);
 
 
 /* Globals */
 int primesFound         = 0;
 int count               = 2;
 int maxCount            = 1000000;
+int digits[6];
 
 
 int main()
@@ -59,14 +64,12 @@ int primecheck(int check){
     if (check % 2 == 0) return 0;
 
     int num = 3;
-    int limit = num * num;     // store the square once
-
-    while (limit <= check) {
+    
+    while ((num * num) <= check) {
         if (check % num == 0){
             return 0;
         } 
         num += 2;               //Skip even numbers
-        limit = num * num;      // update once each iteration
     }
 
     return 1;
@@ -76,17 +79,71 @@ int primecheck(int check){
 /* Rearange number and send it to primecheck() Return 0 (no) or 1 (yes) */
 int circlecheck(int num){
     int circle = 0;
-    //Do some iteration stuff
-
-    if (primecheck(num) == 1){
-        circle = 1;
-        //Go to next iteration
+    int check  = 0;
+    int numDigits = numPlaces(num);
+    int combos = factorial(numDigits) - 1;  //We have one already, its the one that got us in here
+    singledigits(num, numDigits);           //break it into digits, and store in the array
+    for (int x = numDigits; x >= 0; x--){   //if theres any even numbers, its not going to work
+        if (digits[x] % 2 == 0) return 0;
     }
-    else {
-        circle = 0;
-        return circle;
+
+    //Do some iteration stuff
+    for (combos; combos >= 0; combos--){
+        //I need to do some combo stuff????
+
+
+        if (primecheck(digitsToInt(numDigits)) == 1){
+            circle = 1;
+            //Go to next iteration
+        }
+        else {
+            circle = 0;
+            return circle;
+        }
     }
 
     return circle;
 }
 
+/* For calcualting how many digits we are using */
+int numPlaces(int n){
+    if (n < 10) return 1;
+    if (n < 100) return 2;
+    if (n < 1000) return 3;
+    if (n < 10000) return 4;
+    if (n < 100000) return 5;
+    if (n < 1000000) return 6;
+    if (n < 10000000) return 7;
+    if (n < 100000000) return 8;
+    if (n < 1000000000) return 9;
+    return 10;
+}
+
+/* Get Factorial for number of combinations we have to check */
+int factorial(int x){
+    int y = 1;
+    for (x; x > 0; x--){
+        y *= x;
+    }
+    return y;
+}
+
+/* Break a number into its digits and store in an array digits[6] */
+void singledigits(int num, int x){
+    int y = 1;
+    for (x; x >= 0; x --){
+        digits[x] = num / y % 10;
+        y *= 10;
+    }
+}
+
+/* Turn the array back into a normal int, input is numDigits */
+int digitsToInt(int x){
+    int y = 0;
+    int z = 1;
+    for (x; x >= 0; x--){
+        y = digits[x] * z;
+        z *= 10;
+    }
+    return y;
+}
