da4a75bab6192f5aa03617288bc5e0030234d48f
Lornop <lorenoelectric@gmail.com>
2025-11-27 18:30:20 -0800

    35 is done.

--- problem35.c
+++ problem35.c
@@ -21,11 +21,12 @@ int numPlaces(int);
 int factorial(int);
 void singledigits(int, int);
 int digitsToInt(int);
+void rotater(int);
 
 
 /* Globals */
-int primesFound         = 0;
-int count               = 2;
+int primesFound         = 1;        //I have to count 2, its a wierd one
+int count               = 3;
 int maxCount            = 1000000;
 int digits[6];
 
@@ -33,12 +34,12 @@ int digits[6];
 int main()
 {
     while(count < maxCount){
-        count ++;
         if (primecheck(count) == 1){
             if (circlecheck(count) == 1){
                 foundOne(count);
             }
         }
+        count += 2;
     }
 
     allDone(primesFound);
@@ -78,31 +79,21 @@ int primecheck(int check){
 
 /* Rearange number and send it to primecheck() Return 0 (no) or 1 (yes) */
 int circlecheck(int num){
-    int circle = 0;
     int check  = 0;
     int numDigits = numPlaces(num);
-    int combos = factorial(numDigits) - 1;  //We have one already, its the one that got us in here
     singledigits(num, numDigits);           //break it into digits, and store in the array
-    for (int x = numDigits; x >= 0; x--){   //if theres any even numbers, its not going to work
+    for (int x = 0; x < numDigits; x++){   //if theres any even numbers, its not going to work
         if (digits[x] % 2 == 0) return 0;
     }
 
-    //Do some iteration stuff
-    for (combos; combos >= 0; combos--){
-        //I need to do some combo stuff????
-
-
-        if (primecheck(digitsToInt(numDigits)) == 1){
-            circle = 1;
-            //Go to next iteration
-        }
-        else {
-            circle = 0;
-            return circle;
+    for (int i = 0; i < numDigits; i++){
+        if (numDigits > 1) rotater(numDigits);
+        if (primecheck(digitsToInt(numDigits)) != 1){
+            return 0;   // early exit if any rotation is not prime
         }
     }
 
-    return circle;
+    return 1;
 }
 
 /* For calcualting how many digits we are using */
@@ -128,10 +119,10 @@ int factorial(int x){
     return y;
 }
 
-/* Break a number into its digits and store in an array digits[6] */
-void singledigits(int num, int x){
+/* Break a number into its digits and store in an array digits[6] number, and how many digits*/
+void singledigits(int num, int digs){
     int y = 1;
-    for (x; x >= 0; x --){
+    for (int x =0; x < digs; x ++){
         digits[x] = num / y % 10;
         y *= 10;
     }
@@ -141,9 +132,20 @@ void singledigits(int num, int x){
 int digitsToInt(int x){
     int y = 0;
     int z = 1;
-    for (x; x >= 0; x--){
-        y = digits[x] * z;
+    for (int a = 0; a < x ; a++){
+        y += digits[a] * z;
         z *= 10;
     }
     return y;
+}
+
+/* Rotate the first digit of the array to the last */
+void rotater(int num){
+    int first = digits[0];            // store the first digit
+
+    for (int i = 0; i < num - 1; i++) {
+        digits[i] = digits[i + 1];    // shift everything left
+    }
+
+    digits[num - 1] = first;          // put the first digit at the end
 }
