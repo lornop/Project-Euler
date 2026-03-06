89f93919a34f4fdcbbb10dd9809b8f835d80d75d
Lornop <lorenoelectric@gmail.com>
2026-03-05 21:31:28 -0800

    Starting 48

---
+++ problem48.c
@@ -0,0 +1,21 @@
+/*
+Self Powers
+Probelm 48
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000
+*/
+
+/* Libraries */
+#include <stdio.h>
+
+/* Function Prototypes */
+
+/* Globals */
+
+int main()
+{
+
+return 0;
+}
+
+/* */
