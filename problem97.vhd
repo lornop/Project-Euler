fa6b6458ede93ca65c84247c0fe90b171c479e63
Lornop <lorenoelectric@gmail.com>
2026-03-08 22:02:58 -0700

    well vhdl is almost done being coded

--- problem97.vhd
+++ problem97.vhd
@@ -28,12 +28,13 @@ entity Large_Non_Mersenne_Prime is
         clk     : in std_logic;   -- clock
         reset   : in std_logic;   -- reset
         key0    : in std_logic;   -- push button
-        HEX0 : out std_logic_vector(6 downto 0);
-        HEX1 : out std_logic_vector(6 downto 0);
-        HEX2 : out std_logic_vector(6 downto 0);
-        HEX3 : out std_logic_vector(6 downto 0);
-        HEX4 : out std_logic_vector(6 downto 0);
-        HEX5 : out std_logic_vector(6 downto 0)
+        HEX0 : out std_logic_vector(7 downto 0);
+        HEX1 : out std_logic_vector(7 downto 0);
+        HEX2 : out std_logic_vector(7 downto 0);
+        HEX3 : out std_logic_vector(7 downto 0);
+        HEX4 : out std_logic_vector(7 downto 0);
+        HEX5 : out std_logic_vector(7 downto 0)
+        
     );
 end entity Large_Non_Mersenne_Prime;
 
@@ -45,28 +46,161 @@ architecture behavioral of Large_Non_Mersenne_Prime is
 
     -- internal signals go here
     -- think of these like your local variables
+    signal exp      :   integer := 7830457;
+    signal result   :   unsigned(39 downto 0) := to_unsigned(1, 40);
+    signal base     :   unsigned(39 downto 0) := to_unsigned(2, 40);
+    signal done     :   std_logic := '0';
+    signal digit0   :   integer range 0 to 9;
+    signal digit1   :   integer range 0 to 9;
+    signal digit2   :   integer range 0 to 9;
+    signal digit3   :   integer range 0 to 9;
+    signal digit4   :   integer range 0 to 9;
+    signal digit5   :   integer range 0 to 11;
+    signal debounce :   integer := 500000;
+    signal timer    :   integer := 0;
+    signal display_upper : std_logic := '0';
+    
 
 begin
 
-    -- ----------------------------------------
-    -- Sequential process: things that happen on clock edges
-    -- think of this like your loops and state changes
-    -- ----------------------------------------
     process(clk, reset)
     begin
-        if reset = '1' then
-            -- reset everything to initial values
+        
+        if reset = '1' then     -- reset everything to initial values
+            exp <= 7830457;
+            result <= to_unsigned(1, 40);
+            base <= to_unsigned(2, 40);
+            done <= '0';
 
-        elsif rising_edge(clk) then
-            -- main logic goes here
+        elsif rising_edge(clk) then     -- main logic goes here
+            if done = '0' then
+                if (exp and 1) = 1 then
+                result <= (result * base) mod 10000000000;
+                end if;
+            base <= (base * base) mod 10000000000;
+            exp <= exp / 2;
+            if exp / 2 = 0 then
+                done <= '1';
+                result <= ((result * 28433) + 1) mod 10000000000;
+            end if;
+                
+            elsif done = '1' then
+                if display_upper = '0' then
+                    digit0 <= result mod 10;
+                    digit1 <= result / 10 mod 10;
+                    digit2 <= result / 100 mod 10;
+                    digit3 <= result / 1000 mod 10;
+                    digit4 <= result / 10000 mod 10;
+                    digit5 <= 10;
+                elsif display_upper = '1' then
+                    digit0 <= result / 100000 mod 10;
+                    digit1 <= result / 1000000 mod 10;
+                    digit2 <= result / 10000000 mod 10;
+                    digit3 <= result / 100000000 mod 10;
+                    digit4 <= result / 1000000000 mod 10;
+                    digit5 <= 11;
+                end if;
 
+                case digit0 is
+                    when 0 => HEX0 <= "11000000";
+                    when 1 => HEX0 <= "11111001";
+                    when 2 => HEX0 <= "10100100";
+                    when 3 => HEX0 <= "10110000";
+                    when 4 => HEX0 <= "10011001";
+                    when 5 => HEX0 <= "10010010";
+                    when 6 => HEX0 <= "10000010";
+                    when 7 => HEX0 <= "11111000";
+                    when 8 => HEX0 <= "10000000";
+                    when 9 => HEX0 <= "10010000";
+                    when others => HEX0 <= "11111111";
+                end case;
+                case digit1 is
+                    when 0 => HEX1 <= "11000000";
+                    when 1 => HEX1 <= "11111001";
+                    when 2 => HEX1 <= "10100100";
+                    when 3 => HEX1 <= "10110000";
+                    when 4 => HEX1 <= "10011001";
+                    when 5 => HEX1 <= "10010010";
+                    when 6 => HEX1 <= "10000010";
+                    when 7 => HEX1 <= "11111000";
+                    when 8 => HEX1 <= "10000000";
+                    when 9 => HEX1 <= "10010000";
+                    when others => HEX1 <= "11111111";
+                end case;
+                case digit2 is
+                    when 0 => HEX2 <= "11000000";
+                    when 1 => HEX2 <= "11111001";
+                    when 2 => HEX2 <= "10100100";
+                    when 3 => HEX2 <= "10110000";
+                    when 4 => HEX2 <= "10011001";
+                    when 5 => HEX2 <= "10010010";
+                    when 6 => HEX2 <= "10000010";
+                    when 7 => HEX2 <= "11111000";
+                    when 8 => HEX2 <= "10000000";
+                    when 9 => HEX2 <= "10010000";
+                    when others => HEX2 <= "11111111";
+                end case;
+                case digit3 is
+                    when 0 => HEX3 <= "11000000";
+                    when 1 => HEX3 <= "11111001";
+                    when 2 => HEX3 <= "10100100";
+                    when 3 => HEX3 <= "10110000";
+                    when 4 => HEX3 <= "10011001";
+                    when 5 => HEX3 <= "10010010";
+                    when 6 => HEX3 <= "10000010";
+                    when 7 => HEX3 <= "11111000";
+                    when 8 => HEX3 <= "10000000";
+                    when 9 => HEX3 <= "10010000";
+                    when others => HEX3 <= "11111111";
+                end case;
+                case digit4 is
+                    when 0 => HEX4 <= "11000000";
+                    when 1 => HEX4 <= "11111001";
+                    when 2 => HEX4 <= "10100100";
+                    when 3 => HEX4 <= "10110000";
+                    when 4 => HEX4 <= "10011001";
+                    when 5 => HEX4 <= "10010010";
+                    when 6 => HEX4 <= "10000010";
+                    when 7 => HEX4 <= "11111000";
+                    when 8 => HEX4 <= "10000000";
+                    when 9 => HEX4 <= "10010000";
+                    when others => HEX4 <= "11111111";
+                end case;
+                case digit5 is
+                    when 0 => HEX5 <= "11000000";
+                    when 1 => HEX5 <= "11111001";
+                    when 2 => HEX5 <= "10100100";
+                    when 3 => HEX5 <= "10110000";
+                    when 4 => HEX5 <= "10011001";
+                    when 5 => HEX5 <= "10010010";
+                    when 6 => HEX5 <= "10000010";
+                    when 7 => HEX5 <= "11111000";
+                    when 8 => HEX5 <= "10000000";
+                    when 9 => HEX5 <= "10010000";
+                    when 10 => HEX5 <= "01111111";
+                    when others => HEX5 <= "11111111";
+                end case;
+            end if;
         end if;
     end process;
 
-    -- ----------------------------------------
-    -- Combinational logic goes outside the process
-    -- think of this like expressions that are always true
-    -- wires rather than registers
-    -- ----------------------------------------
+    process(clk, key0)
+    begin
+    if rising_edge(clk) then
+        if key0 = '0' then
+        timer <= timer + 1;
+        elsif key0 = '1' then
+            timer <= 0;
+        end if;
+
+        if timer = debounce then
+            display_upper <= not display_upper;
+            timer <= debounce;
+        end if;
+    end if;
+
+
+    end process;
+
 
 end behavioral;
